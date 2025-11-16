f954ec805922cb1ea400e3aac87b011226486fa6
Nicolas Lagos <jnlagos10@gmail.com>
2025-11-16 15:51:41 -0500

    Agregar estructura inicial del proyecto

---
+++ Backend/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy
+import matplotlib
+import librosa
+import soundfile as sf
+
+print("NumPy OK")
+print("SciPy OK")
+print("Matplotlib OK")
+print("Librosa OK")
+print("SoundFile OK")
